88e77a28e02fdd9db7e7c771ccae0cc01537e9de
marvelTtihu <abangpung01@gmail.com>
2026-09-24 00:14:51 +0800

    feat: add 5 different state

---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const ExpenseForm = () => {
+        const[name, setName] = useState("");
+        const[category, setCategory] = useState("");
+        const[type, setType] = useState("income");
+        const[amount, setAmount] = useState("");
+        const[desc, setDesc] = useState("");
+
+    return(
+        <></>
+    );
+}
+
+export default ExpenseForm;
